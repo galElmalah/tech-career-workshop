bdec29961bdc85d4222ad7f8f18b61cba31b4b9a
Gal Elmalah <gale@wix.com>
2021-10-25 14:18:46 +0300

    remove impl

--- src/matrices/independent/spiralOfDoom.ts
+++ src/matrices/independent/spiralOfDoom.ts
@@ -1,15 +1,1 @@
-export const spiralOfDom = (matrix: number[][]) => {
-  const [top, right, bottom, left] = [0, matrix[0].length - 1, matrix.length - 1, 0];
-  const spiral = [];
-  while (true) {
-    for (let i = top; i < matrix[0].length - top; i++) {
-      spiral.push(matrix[top][i]);
-    }
-    top++
-
-    for (let i = right; i > matrix.length - right; i++) {
-      spiral.push(matrix[i][right]);
-    }
-    top++
-  }
-};
+export const spiralOfDom = (matrix: number[][]) => {};
